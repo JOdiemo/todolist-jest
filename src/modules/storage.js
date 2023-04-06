817487b1f48d7a168d5bf280c1ea62dbedbecaf6
JOdiemo <odiemo@gmail.com>
2023-04-06 03:29:03 +0300

    Added issue

---
+++ src/modules/storage.js
@@ -0,0 +1,26 @@
+class ToDo {
+  constructor(description) {
+    this.description = description;
+  }
+
+  addTodo = () => {
+    const data = Todo.getTodo();
+    const index = Todo.getIndex();
+    const todo = {
+      index,
+      description: this.description,
+      completed: false,
+    };
+
+    if (data === null) {
+      data.push(todo);
+      localStorage.setItem('todo', JSON.stringify(data));
+    }
+    let newtodoList = JSON.parse(localStorage.getItem('todo'));
+    newtodoList = [...data, todo];
+    localStorage.setItem('todo', JSON.stringify(newtodoList));
+    Todo.clearInput();
+    Todo.display();
+    Todo.updateIndex();
+  }
+}
